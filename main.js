d2d659b431c07200958772f797bac470a4c5d9f2
fdfcfc <gorsuv2008@gmail.com>
2024-11-27 21:59:14 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,99 +1,79 @@
-/**
- * A simple extension that downloads an image and then converts it to grey scale using `sharp` node.js lib via
- * Phoenix Code node.js Extension. Extension can be activated using menu: `file->Download Image & Greyscale`.
- * */
-
-/*global define, brackets, $ */
-
-// See detailed docs in https://docs.phcode.dev/api/creating-extensions
-// A good place to look for code examples for extensions: https://github.com/phcode-dev/phoenix/tree/main/src/extensions/default
-
-
 define(function (require, exports, module) {
     "use strict";
 
-    // Brackets modules
     const AppInit = brackets.getModule("utils/AppInit"),
-        DefaultDialogs = brackets.getModule("widgets/DefaultDialogs"),
-        Dialogs = brackets.getModule("widgets/Dialogs"),
         CommandManager = brackets.getModule("command/CommandManager"),
         Menus = brackets.getModule("command/Menus"),
+        ExtensionUtils = brackets.getModule("utils/ExtensionUtils"),
+        PanelManager = brackets.getModule("view/PanelManager"),
         NodeConnector = brackets.getModule("NodeConnector");
 
-    let nodeConnector;
+    const NODE_CONNECTOR_ID = "ext_main_shell";
+    let nodeConnector = NodeConnector.createNodeConnector(NODE_CONNECTOR_ID, exports);
 
-    async function fetchImage() {
-        const imageUrl = "https://picsum.photos/536/354";
-        const response = await fetch(imageUrl);
+    var toolbar = require("text!html/toolbar.html");
 
-        if (!response.ok) {
-            throw new Error(
-                `Failed to fetch image (status ${response.status})`
-            );
+    var panelHtml = require("text!html/panel.html");
+
+    var shellPanel = PanelManager.createBottomPanel("shell.panel", $(panelHtml), 100);
+    shellPanel.hide();
+
+    $("#close-shell").on("click", function () {
+        shellPanel.hide();
+    });
+
+    var COMMAND_ID = "shell.toggle";
+    CommandManager.register("Toggle Shell", COMMAND_ID, function () {
+        if (shellPanel.isVisible()) {
+            shellPanel.hide();
+        } else {
+            shellPanel.show();
         }
+    });
 
-        return response.arrayBuffer();
-    }
+    var menu = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU);
+    menu.addMenuItem(COMMAND_ID);
 
-    // Function to run when the menu item is clicked
-    async function handleHelloWorld() {
-        if (!Phoenix.isNativeApp) {
-            alert("Node Features only works in desktop apps.");
-            return;
+    $("#main-toolbar .buttons").append(toolbar);
+    $("#main-toolbar").on("click", function () {
+        if (shellPanel.isVisible()) {
+            shellPanel.hide();
+        } else {
+            shellPanel.show();
         }
-        let html = "<b>Image conversion failed</b>";
-        try {
-            alert("downloading image...");
-            // Fetch the image and get its array buffer
-            const imageArrayBuffer = await fetchImage();
+    });
 
-            // Call the nodeConnector to convert the image to grayscale
-            const { buffer, success } = await nodeConnector.execPeer(
-                "convertToGreyScale",
-                { imageName: "imageName" },
-                imageArrayBuffer
-            );
-
-            if (!success) {
-                alert("Image conversion failed in Node.");
-                return;
-            }
-
-            // Construct HTML with the grayscale image array buffer
-            // For example, you can use the buffer as a base64 data URL
-            html = `<img src="data:image/jpeg;base64,${Buffer.from(buffer).toString("base64")}">`;
-        } catch (error) {
-            console.error("Error:", error);
-        }
-        Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_INFO, "Image to greyscale with node.js", html);
-    }
-
-    // First, register a command - a UI-less object associating an id to a handler
-    var MY_COMMAND_ID = "helloworld.imageConvert"; // package-style naming to avoid collisions
-    CommandManager.register("Download Image & Greyscale", MY_COMMAND_ID, handleHelloWorld);
-
-    // Then create a menu item bound to the command
-    // The label of the menu item is the name we gave the command (see above)
-    var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
-    menu.addMenuItem(MY_COMMAND_ID);
-
-    // We could also add a key binding at the same time:
-    //menu.addMenuItem(MY_COMMAND_ID, "Ctrl-Alt-W");
-    // (Note: "Ctrl" is automatically mapped to "Cmd" on Mac)
-
-    // Initialize extension once shell is finished initializing.
     AppInit.appReady(function () {
-        // nb: Please enable `Debug menu> Phoenix code diagnostic tools> enable detailed logs` to view all console logs.`
-        console.log("hello world");
+        ExtensionUtils.loadStyleSheet(module, "styles/main.css");
 
-        if (Phoenix.isNativeApp) {
-            nodeConnector = NodeConnector.createNodeConnector(
-                "your-extension-id-1",
-                exports
-            );
-            // you can also execute nodejs code in dekstop builds
-            // below code will execute the function `echoTest` defined in `node/index.js`
-            nodeConnector.execPeer("echoTest", "yo!").then(console.log);
-        }
+        let clearCmds = ["cls", "clean", "clear"];
+        $("#shell-input").on("keypress", async function (e) {
+            if (e.key === "Enter") {
+                e.preventDefault();
+                var input = await $(this).val().trim();
+                $(this).val("");
+
+                if (clearCmds.includes(input)) {
+                    $("#shell-output").html("");
+                } else {
+                    let dir = "";
+                    nodeConnector
+                        .execPeer("execute", { input })
+                        .then((result) => {
+                            let msg = JSON.parse(result).msg;
+                            dir = JSON.parse(result).dir;
+                            $("#shell-output").append(`<div>${dir}&gt; ${input}</div>`);
+                            $("#shell-output").append(`<div>${msg}</div>`);
+                            $("#shell-output").append(`<div></div>`);
+                        })
+                        .catch((err) => {
+                            console.log("err", err);
+                            $("#shell-output").append(`<div>${dir}&gt; ${input}</div>`);
+                            $("#shell-output").append(`<div style="color: red;">${err.message}</div>`);
+                            $("#shell-output").append(`<div></div>`);
+                        });
+                }
+            }
+        });
     });
 });
